e93252d6dbfa65fcd7729789637931e2c5d72641
KpuTuk <37570278+x7s@users.noreply.github.com>
2025-03-13 20:17:47 +0200

    Add files via upload

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,3 +4,10 @@ module.exports = (req, res, next) => {
 	}
 	next();
 };
+
+module.exports = (req, res, next) => {
+	if (!req.session.publisher) {
+	  return res.redirect('/publishers/login');
+	}
+	next();
+};
