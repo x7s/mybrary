0d268f74eb578341c1016fa00271ef2b720b2eaf
KpuTuk <37570278+x7s@users.noreply.github.com>
2025-03-18 18:06:12 +0200

    Enhanced password hashing security in Admin.js using bcrypt with 12 salt rounds.

--- models/Admin.js
+++ models/Admin.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const adminSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+adminSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
 });
 
 // Hash password before saving
