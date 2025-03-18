d3c02690437eb6b7086fe3a64e3ad371fd162fab
KpuTuk <37570278+x7s@users.noreply.github.com>
2025-03-18 18:08:31 +0200

    Enhanced password hashing security in Publisher.js using bcrypt with 12 salt rounds.

--- models/Publisher.js
+++ models/Publisher.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const publisherSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+publisherSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
 	bio: { type: String },
 });
 
