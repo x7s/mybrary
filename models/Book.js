7c38fbd35cb6f55a4ff25fd501b560e5a44c295a
KpuTuk <37570278+x7s@users.noreply.github.com>
2025-03-27 21:25:59 +0200

    Add files via upload

--- models/Book.js
+++ models/Book.js
@@ -8,6 +8,8 @@ const bookSchema = new mongoose.Schema(
 		pageCount: { type: Number, required: true },
 		coverImage: { type: Buffer },
 		coverImageType: { type: String },
+		// 📌 Запазваме само пътя
+		coverImagePath: { type: String, required: true },
 		author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
 		publisher: { type: mongoose.Schema.Types.ObjectId, ref: 'Publisher', required: true },
 	},
@@ -15,10 +17,16 @@ const bookSchema = new mongoose.Schema(
 	{ timestamps: true },
 );
 
+// 📌 Virtual property за извличане на пълния път до изображението
+
+bookSchema.virtual('coverImageUrl').get(function() {
+	return `/uploads/${this.coverImagePath}`;
+});
+/*
 bookSchema.virtual('coverImagePath').get(function() {
 	if (this.coverImage != null && this.coverImageType != null) {
 		return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`;
 	}
 });
-
+*/
 module.exports = mongoose.model('Book', bookSchema);
